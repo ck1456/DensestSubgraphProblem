ba5be09861e0ff1225789ed679017a5dca77abc2
ck1456 <ck1456@nyu.edu>
2014-12-04 16:05:30 -0500

    Fixed testing script.

--- test_and_score_all.sh
+++ test_and_score_all.sh
@@ -5,18 +5,15 @@ solndir="soln/"
 invoke="java -jar LemonFinder.jar"
 rm -rf "${solndir}"
 
-for p in 0 1 2 3 
-do
-  for n in 0 1 2
+for n in 0 1 2 3 4 5 6  7 8 9 10 11 
   do
-  infile="${datadir}problem${p}_input_${n}.txt"
-  outfile="${solndir}problem${p}_output_${n}.txt"
-  paramfile="${datadir}problem${p}_param_${n}.txt"
+  infile="${datadir}input_${n}.txt"
+  outfile="${solndir}output_${n}.txt"
+  paramfile="${datadir}param_${n}.txt"
   echo "${infile}"
   ${invoke} ${infile} ${outfile}
 
   java -jar ./spec/evaluate.jar "${paramfile}" "${outfile}"
-  done
 done
 echo
 
