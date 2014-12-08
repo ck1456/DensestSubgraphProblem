9271f2d7a4475f1b342f6317c25ff9b1fab5eebc
ck1456 <ck1456@nyu.edu>
2014-12-08 00:43:25 -0500

    Final typos.

--- test_and_score_all.sh
+++ test_and_score_all.sh
@@ -5,13 +5,13 @@ solndir="soln/"
 invoke="java -jar LemonFinder.jar"
 rm -rf "${solndir}"
 
-for n in 0 1 2 3 4 5 6  7 8 9 10 11 
+for n in 0 1 2 3 4 5 6 7 8 9 10 11 
   do
   infile="${datadir}input_${n}.txt"
   outfile="${solndir}output_${n}.txt"
   paramfile="${datadir}param_${n}.txt"
   echo "${infile}"
-  time ${invoke} ${infile} ${outfile}
+  ${invoke} ${infile} ${outfile}
 
   java -jar ./spec/evaluate.jar "${paramfile}" "${outfile}"
 done
